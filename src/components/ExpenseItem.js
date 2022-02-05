1b7c50e186d5e6a4c2ca5c3c976fa1b27632ef5e
MD Ashik Ashrafe <ashik.ashrafee@selise.ch>
2022-02-05 11:32:19 +0600

    initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+import './ExpenseItem.css'
+import ExpenseDate from'./ExpenseDate';
+import Card from './Card';
+
+function ExpenseItem(props){    
+    const {title,amount,date} = props?.data;
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate date = {date}/>
+            <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>${amount}</div>
+            </div>
+        </Card>
+    )
+}
+
+export default ExpenseItem;
---
+++ src/components/Expenses.js
@@ -0,0 +1,16 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "./Card";
+
+function Expenses(props) {      
+  const { expenses } = props;
+  return (
+    <Card className="expenses">
+      {expenses.map((expense, index) => {
+        return <ExpenseItem key={index} data={expense} />;
+      })}
+    </Card>
+  );
+}
+
+export default Expenses;
